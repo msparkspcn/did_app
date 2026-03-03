0b5d45ff9e86bfd179d6d2c3e78c3756525cfe32
minsupark <msparkspcn@gmail.com>
2026-03-03 13:53:32 +0900

    chore: stop tracking app build artifacts

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_secta9ine_didapp_DidApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.secta9ine.didapp.DidApplication"
-)
-public final class _com_secta9ine_didapp_DidApplication {
-}
